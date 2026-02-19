ed1bec2aeb9eb45c2a33aff853067c7d26648851
Faust0z <fausto.z77@hotmail.com>
2026-02-19 12:21:45 -0300

    Run tests

--- src/test/java/com/faust0z/BookLibraryAPI/BookLibraryAPIApplicationTests.java
+++ src/test/java/com/faust0z/BookLibraryAPI/BookLibraryAPIApplicationTests.java
@@ -2,8 +2,10 @@ package com.faust0z.BookLibraryAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookLibraryAPIApplicationTests {
 
     @Test
